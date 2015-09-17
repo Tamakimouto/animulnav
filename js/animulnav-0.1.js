6de9517ca0485c29cb223d3ff9118505901f84e8
Anthony Zheng <tamakimouto@gmail.com>
2015-09-17 15:00:25 -0400

    test

--- js/animulnav-0.1.js
+++ js/animulnav-0.1.js
@@ -10,16 +10,16 @@
 (function( $ ){
 
   $.fn.animulnav = function() {
-  	
+
   	// Find the ul element
     var div = this,
     	ul = this.find('ul'),
     	ulPadding = 15;
-    	
-    //Get the element width and the last li child
+
+    // Get the element width and the last li child
     var divWidth = this.width();
-	var lastLi = ul.find('li:last-child');	
-    
+	var lastLi = ul.find('li:last-child');
+
     //Create the animation
     div.mousemove(function(e){
     	var ulWidth = lastLi[0].offsetLeft + lastLi.outerWidth() + ulPadding;
